23e58c7fd34876d7261504dae3b8e835984f07d4
Thierry Lacour <tlacour@verifa.io>
2022-10-21 12:41:14 +0200

    Vaporize dead code

--- ui/src/routes/+layout.svelte
+++ ui/src/routes/+layout.svelte
@@ -17,6 +17,7 @@
 	$: isLoginPage = (): boolean => {
 		return $page.url.pathname === `${base}/login`;
 	};
+
 	authStore.subscribe((resp) => {
 		// Do nothing if request is in progress
 		if (resp.fetching) {
@@ -31,18 +32,6 @@
 	authStore.get('/authenticate');
 
 	onMount(() => {
-		if ('theme' in localStorage) {
-			const theme = localStorage['theme'];
-			if (theme === 'dark') {
-				darkTheme = true;
-			} else {
-				darkTheme = false;
-			}
-		} else {
-			// Get preference for dark theme from browser
-			darkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
-		}
-
 		// Subscribe to the session store to handle events if user is not authenticated
 		session.subscribe((session) => {
 			if (!session.initialized) {
@@ -59,18 +48,6 @@
 		});
 	});
 
-	let topAppBar: TopAppBar;
-	let darkTheme: boolean = false;
-
-	$: modeLabel = `switch to ${darkTheme ? 'light' : 'dark'} mode`;
-	$: modeIcon = darkTheme ? 'light_mode' : 'dark_mode';
-
-	function toggleMode() {
-		darkTheme = !darkTheme;
-		// Remember choice: store the theme setting in local storage
-		localStorage.setItem('theme', darkTheme ? 'dark' : 'light');
-	}
-
 	function handleLogout() {
 		logoutStore.get('/logout');
 		logoutStore.subscribe((value) => {
@@ -84,28 +61,6 @@
 <!-- Configure TailwindCSS typographic defaults for whole site -->
 <div class="prose mx-auto max-w-full">
 	<NavBar />
-
-	<!-- <TopAppBar bind:this={topAppBar} variant="fixed">
-		<Row>
-			<Section>
-				<IconButton class="material-icons">menu</IconButton>
-				<a href={base}><Title>Coastline</Title></a>
-			</Section>
-			<Section align={'end'}>
-				<IconButton
-				aria-label={modeLabel}
-				class="material-icons"
-				on:click={toggleMode}
-				title={modeLabel}
-				>
-				{modeIcon}
-			</IconButton>
-			<Button on:click={handleLogout}>
-				<Label>Logout</Label>
-			</Button>
-		</Section>
-	</Row>
-</TopAppBar> -->
 	<div class="relative mx-auto max-w-full md:px-6 mt-6">
 		{#if $authStore.fetching}
 			<h2>Authenticating...</h2>
@@ -118,11 +73,3 @@
 		{/if}
 	</div>
 </div>
-
-<!-- <svelte:head>
-	{#if darkTheme}
-		<link rel="stylesheet" href="{base}/smui-dark.css" media="screen" />
-	{:else}
-		<link rel="stylesheet" href="{base}/smui.css" />
-	{/if}
-</svelte:head> -->
